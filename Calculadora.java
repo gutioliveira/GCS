0baa26a7e866db9d92d9b9908882a5ecd32ff490
MaxOliveira <maxwell_oliveira2@hotmail.com>
2016-09-12 17:45:09 -0300

    metodo para subtracao

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,7 @@
 public class Main{
 	public static void main(String[] args){
-
+		Subtracao subtracao = new Subtracao();
+		subtracao.subtracao(100, 50);
 
 
 		return 0;
---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+
+public void subtracao(float primeiroNumero, float segundoNumero){
+	float resultadoSubtracao = primeiroNumero - segundoNumero;
+	return resultadoSubtracao;
+}
+
+
+	public static void main(String[] args){
+
+
+
+		return 0;
+	}	
+
+}
